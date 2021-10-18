21110f305202673ef59259b3c2a02ff589912531
bestmycode <bestmycode610@gmail.com>
2021-10-17 17:18:53 -0700

    customize solana_candy_factor

---
+++ devtools/dev/setup-solana-dev.sh
@@ -0,0 +1,11 @@
+echo "[INFO] Setting Solana to use dev network"
+solana config set --url https://api.devnet.solana.com
+echo "[INFO] Generating new wallet | or uncomment the to import one"
+solana-keygen new --outfile ~/.config/solana/candyfactory-devnet.json --force && solana address > ./logs/dev/new-wallet-log.txt
+# read answer
+# echo $answer > ~/.config/solana/candyfactory-devnet.json
+echo "[INFO] Setting config to be the new wallet"
+solana config set -k ~/.config/solana/candyfactory-devnet.json
+solana address > ./logs/main/wallet-log.txt
+echo "[INFO] Sending airdrop"
+solana airdrop 5
